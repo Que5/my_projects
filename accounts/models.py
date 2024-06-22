90b8346697eec5dfe1380eebf59d18f36a82d444
Que5 <phumlanibrightdlamini@gmail.com>
2024-06-22 09:43:18 +0200

    UserManagerClass is finished

--- accounts/models.py
+++ accounts/models.py
@@ -10,9 +10,34 @@ class UserManager(BaseUserManager):
             raise ValueError('User must provide a username')
         
         user = self.model(
-            email = self.normalize_email(email)
-
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
         )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, first_name, last_name, username, email, password=None):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+    
+    
+
+
+
 
 
 class User(AbstractBaseUser):
