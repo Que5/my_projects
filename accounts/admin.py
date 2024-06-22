d5cc1011dcee5f2ed77ea9e1a8ce9d88d7b97d70
Que5 <phumlanibrightdlamini@gmail.com>
2024-06-22 13:28:43 +0200

    Made the password read-only on the admin dashboard

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(User, CustomUserAdmin)
