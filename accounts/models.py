fde055d56c417fd709969bceecb2d60d9d69d109
Que5 <phumlanibrightdlamini@gmail.com>
2024-06-23 14:29:03 +0200

    Added the address line

--- accounts/models.py
+++ accounts/models.py
@@ -79,6 +79,8 @@ class User(AbstractBaseUser):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
+    cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
+    address_line = models.CharField(max_length=50, blank=True, null=True)
 
 
 
