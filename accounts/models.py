d1832a08cad5d85f1e7ffbbec65cf2356bf72374
Que5 <phumlanibrightdlamini@gmail.com>
2024-06-24 10:22:25 +0200

    Completed the UserProfile class

--- accounts/models.py
+++ accounts/models.py
@@ -80,7 +80,21 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
     cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
-    address_line = models.CharField(max_length=50, blank=True, null=True)
+    address_line1 = models.CharField(max_length=50, blank=True, null=True)
+    address_line2 = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=15, blank=True, null=True)
+    province = models.CharField(max_length=15, blank=True, null=True)
+    city = models.CharField(max_length=15, blank=True, null=True)
+    postal_code = models.CharField(max_length=4, blank=True, null=True)
+    latitude = models.CharField(max_length=20, blank=True, null=True)
+    longitude = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+def __str__(self):
+    return self.user.email
+
 
 
 
