f1510340928dd268002a6233fa2ab1c21b60b21e
Que5 <phumlanibrightdlamini@gmail.com>
2024-06-24 14:30:54 +0200

    Created signal.py and added a ready function inside apps.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -97,26 +96,4 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.email
     
-
-@receiver(post_save, sender=User)
-def post_save_create_profile_reciever(sender, instance, created, **kwargs):
-    print(created)
-    if created:
-        UserProfile.objects.create(user=instance)
-        print('User profile is created')
-    else:
-        try:
-            profile = UserProfile.objects.get(user=instance)
-            profile.save()
-        except:
-            # Create the userprofile if it does not exist
-            profile = UserProfile.objects.get(user=instance)
-            print('Profile did not exist but I created one.')
-        print('User is updated')
-
-# post_save.connect(post_save_create_profile_reciever, sender=User)
-
-
-
-
 
