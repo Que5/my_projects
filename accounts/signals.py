06c00755206a3b12f396d6408ac2f8bab13ab72e
Que5 <phumlanibrightdlamini@gmail.com>
2024-06-28 08:44:50 +0200

    Removed the print statements

--- accounts/signals.py
+++ accounts/signals.py
@@ -8,7 +8,7 @@ def post_save_create_profile_reciever(sender, instance, created, **kwargs):
     print(created)
     if created:
         UserProfile.objects.create(user=instance)
-        print('User profile is created')
+       
     else:
         try:
             profile = UserProfile.objects.get(user=instance)
@@ -16,12 +16,11 @@ def post_save_create_profile_reciever(sender, instance, created, **kwargs):
         except:
             # Create the userprofile if it does not exist
             profile = UserProfile.objects.get(user=instance)
-            print('Profile did not exist but I created one.')
-        print('User is updated')
+           
 
 
 @receiver(pre_save, sender=User)
 def pre_save_profile_receiver(sender, instance, **kwargs):
-    print(instance.username, 'This user is being saved')
+    pass
 
 # post_save.connect(post_save_create_profile_reciever, sender=User)
