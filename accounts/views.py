155f2fb87f2b742a61cd3003859798b84a2f2c9d
Que5 <phumlanibrightdlamini@gmail.com>
2024-06-28 09:22:30 +0200

    Hashed user password

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,9 @@ def registerUser(request):
         print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
+            password = form.cleaned_data['password']
             user = form.save(commit=False)
+            user.set_password(password)
             user.role = User.CUSTOMER
             form.save()
             return redirect('registerUser')
