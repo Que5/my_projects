27c7b087545a96dff9bdd53c6611b823fee26a7e
Que5 <phumlanibrightdlamini@gmail.com>
2024-06-28 09:13:02 +0200

    Added the role

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import UserForm
+from .models import User
 
 # Create your views here.
 
@@ -9,6 +10,8 @@ def registerUser(request):
         print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
             form.save()
             return redirect('registerUser')
 
